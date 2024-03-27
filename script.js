f1abf5c10812f58700a75e9c37b732ee28376ad1
Group int theprodev <145545861+Istituto-freudinttheprodev@users.noreply.github.com>
2024-03-27 08:10:42 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 // Funzione per aprire il popup quando il documento è pronto
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("popup").style.display = "block";
-    document.body.style.overflow = "hidden"; // Impedisce lo scroll dello sfondo quando il popup è attivo
+    document.body.style.overflow = "hidden"; // Impedisce lo scroll della pagina quando il popup è attivo
   });
   
   // Funzione per chiudere il popup quando si clicca sul pulsante "Chiudi"
-  function closePopup() {
+  function chiudipopup() {
     document.getElementById("popup").style.display = "none";
-    document.body.style.overflow = "auto"; // Riabilita lo scroll dello sfondo quando il popup viene chiuso
+    document.body.style.overflow = "auto"; // Riabilita lo scroll della pagina quando il popup viene chiuso
   }
 
   // Lista di immagini di sfondo
-  const backgroundImageList = [
+  const listaimmaginidisfondo = [
     "TORTA AL CIOCCOLATO 1.png",
     "TORTA AL CIOCCOLATO 2.jpg",
     "PREPARAZIONE TORTA.png",
@@ -22,14 +22,14 @@ document.addEventListener("DOMContentLoaded", function() {
   ];
 
   // Funzione per cambiare l'immagine di sfondo
-  function changeBackgroundImage() {
-    const randomIndex = Math.floor(Math.random() * backgroundImageList.length);
-    const randomImage = backgroundImageList[randomIndex];
-    document.getElementById('homepage').style.backgroundImage = `url('${randomImage}')`;
+  function cambioimmaginedisfondo() {
+    const mumerorandomico = Math.floor(Math.random() * listaimmaginidisfondo.length);
+    const immaginerandomica = listaimmaginidisfondo[mumerorandomico];
+    document.getElementById('homepage').style.backgroundImage = `url('${immaginerandomica}')`;
   }
 
   // Cambia l'immagine di sfondo della homepage ogni 3 secondi (3000 millisecondi)
-  setInterval(changeBackgroundImage, 3000);
+  setInterval(cambioimmaginedisfondo, 3000);
   
   // Cambia l'immagine di sfondo all'avvio della pagina
-  changeBackgroundImage();
+  cambioimmaginedisfondo();
