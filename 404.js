a96644ff66fcfbea36fbb5b010cef18e94e5e9a4
Gruppo di informatica e di robotica "int theprodev;" 2°D INFO <145545861+Istituto-freudinttheprodev@users.noreply.github.com>
2024-09-25 10:55:10 +0200

    Add files via upload

---
+++ 404.js
@@ -0,0 +1,14 @@
+// Disabilita il trascinamento delle immagini in una altra scheda (per togliere la possibilità di copiarle)
+document.querySelectorAll("img").forEach(img => {
+    img.addEventListener("dragstart", (e) => e.preventDefault());
+});
+
+// Previene l'azione predefinita del tasto destro del mouse, quindi di conseguenza non è più possibile salvare immagini o gestire altre opzioni del menù contestuale del tasto destro
+document.addEventListener("contextmenu", function(e) {
+    e.preventDefault(); 
+});
+
+// Forza lo scroll della pagina all'inizio ogni volta che viene ricaricata o quando si lascia la pagina e la si riapre dopo
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+};
